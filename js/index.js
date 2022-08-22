31b7c0378f27c4e20eaa8384fd3516cfaf6caaa9
favdauria <69044006+favdauria@users.noreply.github.com>
2022-08-21 21:28:47 -0300

    login form and validation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
+function emailCheck(){   
+    let email = document.getElementById("floatingInput");
+
+    if( email.value != "" ){
+        email.classList.remove('is-invalid');
+        email.classList.add('is-valid');
+        return true;
+    } else{
+        
+        email.classList.remove('is-valid');
+        email.classList.add('is-invalid');
+        return false;
+    }   
+}
+
+function pwdCheck(){   
+    let password = document.getElementById("floatingPassword");
+
+    if( password.value == "" ){
+        password.classList.remove('is-valid');
+        password.classList.add('is-invalid');
+        return false;
+    } else{
+        password.classList.remove('is-invalid');
+        password.classList.add('is-valid');
+        return true;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "cars.html"
+    document.getElementById("subButton").addEventListener("click", function() {
+        if(emailCheck() && pwdCheck()){
+            window.location(index2.html)
+        }else{
+            emailCheck();
+            pwdCheck();
+        }
+
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+
 });
